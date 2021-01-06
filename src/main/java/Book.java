a643c53ca4388a7317a6ccadbcc2baa825e1f6c1
Ruth Ironside <ruthironside@live.com>
2021-01-06 19:37:52 +0000

    add a hashmap to the genre of the books

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Book {
 
         private String title;
         private String genre;
         private String author;
+
+        public static void main(String[] args) {
+            HashMap<String, Integer> genres = new HashMap<String, Integer>();
+
+            genres.put("Fiction", 20);
+            genres.put("Non Fiction", 30);
+        }
 
         public Book(String title, String genre, String author) {
             this.title = title;
